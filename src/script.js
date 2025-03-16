c4a8bc9417ec7ee66b9c181d81da5a39b3aaacff
mangchohyeon <qgeon0419@gmail.com>
2025-03-16 23:56:06 +0900

    footer 꾸미는 중, way3작성해야함

--- src/script.js
+++ src/script.js
@@ -1,10 +1,18 @@
 // DOMContentLoaded 이벤트 이후 auto-render 실행
 document.addEventListener("DOMContentLoaded", function() {
-    renderMathInElement(document.body, {
-      delimiters: [
-        {left: "$$", right: "$$", display: true},
-        {left: "\\(", right: "\\)", display: false}
-      ]
-    });
+  renderMathInElement(document.body, {
+    delimiters: [
+      {left: "$$", right: "$$", display: true},       // 디스플레이 모드
+      {left: "\\(", right: "\\)", display: false},      // 인라인 모드
+      {left: "$", right: "$", display: false}           // 인라인 모드 (필요시)
+    ]
+  });
 });
-  
+
+const img2 = document.getElementById("img2");
+img2.addEventListener("click", () => {
+  const footer_item1 = document.getElementById('footer_item1');
+  if (footer_item1) {
+    footer_item1.scrollIntoView({ behavior: "instant" });
+  }
+});
